0d6359829421fe793e0cdc50e0d47c670bff0247
Christopher Davison <chris@spoofwear.com>
2016-12-29 14:39:30 -0800

    Initial Commit

---
+++ NSTableViewCocoaBindings/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  NSTableViewCocoaBindings
+//
+//  Created by Christopher Davison on 12/29/16.
+//  Copyright © 2016 Christopher Davison. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ NSTableViewCocoaBindings/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  NSTableViewCocoaBindings
+//
+//  Created by Christopher Davison on 12/29/16.
+//  Copyright © 2016 Christopher Davison. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
